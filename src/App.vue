b56a105d6e9655c9936b21577d9f976b843686b8
Bekzod <bekzodbaratov334@gmail.com>
2023-02-10 17:14:18 +0500

    base

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <h3>salom</h3>
+  <div id="app">
+    <h1>Shopping Cart Example</h1>
+    <hr />
+    <h2>Products</h2>
+    <ProductList />
+    <hr />
+    <ShoppingCart />
+  </div>
 </template>
+
+<script>
+import ProductList from "@/components/ProductList.vue";
+import ShoppingCart from "@/components/ShoppingCart.vue";
+export default {
+  components: { ProductList, ShoppingCart },
+};
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+import { createStore, createLogger } from "vuex";
+import cart from "./modules/cart";
+import products from "./modules/products";
+
+const debug = process.env.NODE_ENV !== "production";
+
+export default createStore({
+  modules: {
+    cart,
+    products,
+  },
+  strict: debug,
+  plugins: debug ? [createLogger()] : [],
+});
